618ee95857be66a00ef894516cd2709ddc4f9e9b
Vitor Gabriel <vitor.g.lisboa2018@gmail.com>
2024-06-27 11:11:42 -0300

    teste com imail da biblioteca faker

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade castasdro', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('Deve criar um novo log/cadastro', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('testevitor123')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)'). should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message'). should('contain','Detalhes da conta modificados com sucesso.')
+
+        
+    });
+
+    it('Deve criar um novo log/cadastro- variaves com emil com nome igual', () => {
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+        var email = faker.internet.email(nome+sobrenome)
+        
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('testevitor123')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)'). should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message'). should('contain','Detalhes da conta modificados com sucesso.')
+   
+    });
+    
+});
